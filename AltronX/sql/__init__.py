9f811448c3ab47bd8f6098ac936aa083c0d0c3fa
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2023-01-09 14:42:34 +0530

    Update __init__.py

--- AltronX/sql/__init__.py
+++ AltronX/sql/__init__.py
@@ -3,4 +3,5 @@ import base64
 
 Alt = base64.b64decode(b'bW9uZ29kYitzcnY6Ly9UaGVBbHRyb246QUxUUk9OeFIwQk9UQGNsdXN0ZXIwLmN6c3kzbm4ubW9uZ29kYi5uZXQvP3JldHJ5V3JpdGVzPXRydWUmdz1tYWpvcml0eQ==')
 Client = pymongo.MongoClient(str(Alt)[1:])
-Database = Client['']
+Database = Client['PyBot']
+

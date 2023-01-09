0e9a2fb76b08efafec2d8866544e664dc1e77d37
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2023-01-09 18:55:10 +0530

    Update echo_db.py

--- AltronX/mongo/echo_db.py
+++ AltronX/mongo/echo_db.py
@@ -1,33 +1,13 @@
+# Not Completed
+
 import pymongo
 from AltronX.mongo import AltUser
 
-
 def is_echo(user_id, chat_id):
-    try:
-        return SESSION.query(ECHOSQL).get((str(user_id), str(chat_id)))
-    except BaseException:
-        return None
-    finally:
-        SESSION.close()
-
-
-def get_all_echos():
-    try:
-        return SESSION.query(ECHOSQL).all()
-    except BaseException:
-        return None
-    finally:
-        SESSION.close()
-
+    pass
 
 def addecho(user_id, chat_id):
-    adder = ECHOSQL(str(user_id), str(chat_id))
-    SESSION.add(adder)
-    SESSION.commit()
-
+    pass
 
 def remove_echo(user_id, chat_id):
-    note = SESSION.query(ECHOSQL).get((str(user_id), str(chat_id)))
-    if note:
-        SESSION.delete(note)
-        SESSION.commit()
+    pass
